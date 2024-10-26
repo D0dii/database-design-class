186e34a830f885057e5d67e887e3ca51fc87955f
Oliwia <okomor21@gmail.com>
2024-10-26 19:34:28 +0200

    names and not null checked

--- schema.sql
+++ schema.sql
@@ -54,7 +54,7 @@ CREATE TABLE "Employee" (
   "date_of_birth" DATE NOT NULL CHECK (date_of_birth <= CURRENT_DATE),
   "address" VARCHAR,
   "gender" VARCHAR CHECK (gender IN ('male', 'female')),
-  "email" VARCHAR CHECK (email ~ '^[^@]+@[^@]+\.[^@]+$'),
+  "email" VARCHAR NOT NULL CHECK (email ~ '^[^@]+@[^@]+\.[^@]+$'),
   "password" VARCHAR NOT NULL,
   "phone_number" VARCHAR
 );
@@ -70,7 +70,7 @@ CREATE TABLE "Student" (
   "gender" VARCHAR CHECK (gender IN ('male', 'female')),
   "student_number" INTEGER CHECK (student_number > 0),
   "status" student_status,
-  "email" VARCHAR CHECK (email ~ '^[^@]+@[^@]+\.[^@]+$'),
+  "email" VARCHAR NOT NULL CHECK (email ~ '^[^@]+@[^@]+\.[^@]+$'),
   "password" VARCHAR NOT NULL,
   "phone_number" VARCHAR
 );
@@ -78,8 +78,8 @@ CREATE TABLE "Student" (
 -- Room Table
 CREATE TABLE "Room" (
   "id" SERIAL PRIMARY KEY,
-  "number" INTEGER CHECK (number > 0),
-  "max_capacity" INTEGER CHECK (max_capacity > 0),
+  "number" INTEGER NOT NULL CHECK (number > 0),
+  "max_capacity" INTEGER NOT NULL CHECK (max_capacity > 0),
   "room_status" VARCHAR,
   "dormitory_id" INTEGER REFERENCES "Dormitory" ("id") ON DELETE RESTRICT
 );
@@ -95,7 +95,6 @@ CREATE TABLE "Equipment" (
   "id" SERIAL PRIMARY KEY,
   "purchase_date" DATE NOT NULL CHECK (purchase_date <= CURRENT_DATE),
   "condition" VARCHAR,
-  "category" VARCHAR,
   "category_id" INTEGER REFERENCES "EquipmentCategory" ("id") ON DELETE RESTRICT,
   "room_id" INTEGER REFERENCES "Room" ("id") ON DELETE RESTRICT
 );
@@ -114,7 +113,7 @@ CREATE TABLE "RentalAgreement" (
   "id" SERIAL PRIMARY KEY,
   "contract_date" DATE NOT NULL CHECK (contract_date <= CURRENT_DATE),
   "termination_date" DATE,
-  "payment" FLOAT CHECK (payment >= 0),
+  "payment" FLOAT NOT NULL CHECK (payment >= 0),
   "employee_id" INTEGER REFERENCES "Employee" ("id") ON DELETE RESTRICT,
   "dormitory_id" INTEGER REFERENCES "Dormitory" ("id") ON DELETE RESTRICT,
   "student_id" INTEGER REFERENCES "Student" ("id") ON DELETE RESTRICT,
@@ -147,10 +146,8 @@ CREATE TABLE "Application" (
 -- Payment Table
 CREATE TABLE "Payment" (
   "id" SERIAL PRIMARY KEY,
-  "value" FLOAT CHECK (value > 0),
+  "value" FLOAT NOT NULL CHECK (value > 0),
   "type" VARCHAR,
   "status" payment_status,
   "student_id" INTEGER REFERENCES "Student" ("id") ON DELETE RESTRICT
 );
-
--- test
